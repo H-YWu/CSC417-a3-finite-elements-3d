7f00d3ffdc097f5a86542fbab67f79580887e108
Haoyang Wu <nn9.yang@gmail.com>
2024-03-19 09:44:04 +0800

    delete: redundant comments

--- src/assemble_stiffness.cpp
+++ src/assemble_stiffness.cpp
@@ -11,11 +11,6 @@ void assemble_stiffness(Eigen::SparseMatrixd &K, Eigen::Ref<const Eigen::VectorX
         Eigen::RowVector4i element = T.row(r);
         Eigen::Matrix1212d H;
         d2V_linear_tetrahedron_dq2(H, q, V, element, v0(r), C, D); 
-        // E^T(3 * idx[i] + j, 3i + j) = 1 for i = 0..3, j = 0..2
-        // m(x, y) for x,y = 0..11
-        //  (m E)(x, 3 * idx[i] + j) += m(x, 3i + j) for x = 0..11, i = 0..3, j = 0..2
-        // M: (E^T m E) (3 * idx[i] + j, 3 * idx[i] + j) += m(3i + j, 3i + j) for i = 0..3, j = 0..2
-
         // K = - \partial^2{V}{q} = - \sum \partial^2{V_r}{q}
         //   = - \sum (E^T {H} E)_r
         //  E(3i + j, 3 * idx[i] + j)   = 1 for i = 0..3, j = 0..2
